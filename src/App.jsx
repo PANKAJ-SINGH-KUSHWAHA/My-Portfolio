781b0c75a02a01dd06afb7ffa480a33653b2ef97
PANKAJ SINGH KUSHWAHA <145460446+PANKAJ-SINGH-KUSHWAHA@users.noreply.github.com>
2025-02-15 19:34:52 +0530

    removed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
-import image from "./images/me.jpg";
+import image from "./images/me.png";
 import "./styles.css";
 
 /**
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/me.jpg";
+import image from "../images/me.png";
 
 const imageAltText = "Pankaj Singh Kushwaha Profile photo";
 
